2f23e319d97a5dc458df43b497d95744ce7a26b7
Abubaker Elsiddig <113717597+Abubaker2030@users.noreply.github.com>
2022-11-04 23:28:55 +0000

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,3 +1,17 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - function that concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ *
+ * File: 0-strcat.c
+ * Auth: Abubaker2030
+ *
+ * Return: pointer dtring dest
+ */
 char *_strcat(char *dest, char *src)
-{}
+{
+	return (strcat(dest, src));
+}
