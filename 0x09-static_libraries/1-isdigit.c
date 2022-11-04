d3ecb852fac3e4b1c429b87b0be98f31f9fc7e3b
Abubaker Elsiddig <113717597+Abubaker2030@users.noreply.github.com>
2022-11-04 23:29:50 +0000

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,3 +1,17 @@
 #include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: number to check
+ *
+ * File: 1-isdigit.c
+ *
+ * Return: 1 if c is a digit 0 otherwise
+ */
 int _isdigit(int c)
-{}
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
