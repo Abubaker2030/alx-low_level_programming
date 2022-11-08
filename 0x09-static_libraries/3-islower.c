db436959230948b259842ee72bbea3e004958f97
Abubaker2030 <bakryabdallh906@gmail.com>
2022-11-08 03:48:05 -0800

    task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * _islower - Write a function that checks for lowercase character
+ *
+ * @c: the character
+ *
+ * File: 3-islower.c
+ *
+ * Auth: Abubaker2030
+ *
+ * Return: Success 0
+ *
+ **/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
